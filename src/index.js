55eb74534b9315c5df4d16648f8df3323ebf80e6
Marcio Silva <marcio.silva@ibm.com>
2023-07-12 09:15:14 -0300

    refactored to use filter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 const friend = (friends) => {
-  out_friends = []
-  for (let i = 0; i < friends.length; i++) {
-    if (friends[i].length === 4) {
-      out_friends.push(friends[i])
-    }
-  }
-
-  return out_friends 
+  return friends.filter(f => f.length === 4);
 };
 
 module.exports = {
